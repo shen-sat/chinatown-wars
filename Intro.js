4801415cb1fffb02e047bd1e242c50fca551e71b
Shen Sat <shensat18@gmail.com>
2019-08-20 12:57:09 +0100

    Comment out code to move to playable LevelOne

--- Intro.js
+++ Intro.js
@@ -18,7 +18,7 @@ class Intro extends Phaser.Scene {
 		this.textBeingWritten = false;
 		this.keyA = this.input.keyboard.addKey('A');
 		this.prevKeyA = false;
-		this.scene.start('LevelOne');
+		// this.scene.start('LevelOne'); THIS SWITCHES TO NEXT SCENE
 	}
 
 	update() {
